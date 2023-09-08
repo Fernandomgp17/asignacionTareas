549c2eb17ead6b28fbd77d99d60a69ff23aa4640
fernando <fernandomgp17@gmail.com>
2023-09-08 11:51:31 -0500

    Agregando estructura de form y envio de datos

--- src/assets/components/todoApp.jsx
+++ src/assets/components/todoApp.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+
 const TodoApp = () => {
+    
+    const [title,setTitle] = useState('');
+    const [todos, setTodos] = useState([]);
+
+    const handleChange = (event) => {
+        setTitle(event.target.value);
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const newTodo = {
+            id: crypto.randomUUID(),
+            title
+        }
+        setTodos([...todos,newTodo]);
+        setTitle('')
+    }
+
+
     return (
-        <div>
-            <h1>hola</h1>
+        <div className="container" >
+            <form className="todoCreateForm" action="" onSubmit={handleSubmit} >
+                <input type="text" className="todoInput" value={title} onChange={handleChange} />
+                <button className="buttonCreate" >Create Todo</button>
+            </form>
+            <div className="container">
+                {todos.map((todo)=>{
+                    return(
+                        <h1 key={todo.id} >{todo.title}</h1>
+                    )
+                })}
+                
+            </div>
         </div>
     )
 }
